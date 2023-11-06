604f854ceda3a0c4ebe6fe68c720af5d29d37de5
TechFoxxo <techfoxxo@protonmail.com>
2023-11-06 08:08:01 +0100

    Added CORS to API

--- ai-stuffs/src/main/kotlin/org/codeberg/techfoxxo/Application.kt
+++ ai-stuffs/src/main/kotlin/org/codeberg/techfoxxo/Application.kt
@@ -3,13 +3,17 @@ package org.codeberg.techfoxxo
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.cors.routing.*
 import org.codeberg.techfoxxo.routes.v1
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-            .start(wait = true)
+        .start(wait = true)
 }
 
 fun Application.module() {
+    install(CORS) {
+        anyHost()
+    }
     v1()
 }
